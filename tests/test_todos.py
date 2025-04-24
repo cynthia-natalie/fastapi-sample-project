2b7c981e6325bdb260fc53eaeec960dfde449bf3
cynthia-natalie <cynthianatalie573@gmail.com>
2025-04-24 21:22:05 +0700

    test_todos file added

---
+++ tests/test_todos.py
@@ -0,0 +1,106 @@
+# Standard library imports...
+from unittest.mock import Mock, patch
+
+# Third-party imports...
+from nose.tools import assert_is_none, assert_list_equal
+
+# Local imports...
+from app.services import get_todos
+
+
+@patch('project.services.requests.get')
+def test_getting_todos_when_response_is_ok(mock_get):
+    todos = [{
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }]
+
+    # Configure the mock to return a response with an OK status code. Also, the mock should have
+    # a `json()` method that returns a list of todos.
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = todos
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_list_equal(response.json(), todos)
+
+
+@patch('project.services.requests.get')
+def test_getting_todos_when_response_is_not_ok(mock_get):
+    # Configure the mock to not return a response with an OK status code.
+    mock_get.return_value.ok = False
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the response contains an error, I should get no todos.
+    assert_is_none(response)
+
+@patch('project.services.get_todos')
+def test_getting_uncompleted_todos_when_todos_is_not_none(mock_get_todos):
+    todo1 = {
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }
+    todo2 = {
+        'userId': 1,
+        'id': 2,
+        'title': 'Walk the dog',
+        'completed': True
+    }
+
+    # Configure mock to return a response with a JSON-serialized list of todos.
+    mock_get_todos.return_value = Mock()
+    mock_get_todos.return_value.json.return_value = [todo1, todo2]
+
+    # Call the service, which will get a list of todos filtered on completed.
+    uncompleted_todos = get_uncompleted_todos()
+
+    # Confirm that the mock was called.
+    assert_true(mock_get_todos.called)
+
+    # Confirm that the expected filtered list of todos was returned.
+    assert_list_equal(uncompleted_todos, [todo1])
+
+
+@patch('project.services.get_todos')
+def test_getting_uncompleted_todos_when_todos_is_none(mock_get_todos):
+    # Configure mock to return None.
+    mock_get_todos.return_value = None
+
+    # Call the service, which will return an empty list.
+    uncompleted_todos = get_uncompleted_todos()
+
+    # Confirm that the mock was called.
+    assert_true(mock_get_todos.called)
+
+    # Confirm that an empty list was returned.
+    assert_list_equal(uncompleted_todos, [])
+
+def test_integration_contract():
+    # Call the service to hit the actual API.
+    actual = get_todos()
+    actual_keys = actual.json().pop().keys()
+
+    # Call the service to hit the mocked API.
+    with patch('project.services.requests.get') as mock_get:
+        mock_get.return_value.ok = True
+        mock_get.return_value.json.return_value = [{
+            'userId': 1,
+            'id': 1,
+            'title': 'Make the bed',
+            'completed': False
+        }]
+
+        mocked = get_todos()
+        mocked_keys = mocked.json().pop().keys()
+
+    # An object from the actual API and an object from the mocked API should have
+    # the same data structure.
+    assert_list_equal(list(actual_keys), list(mocked_keys))
